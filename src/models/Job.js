7ab19a335763dcf5c6162f895acd83ce61587529
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-04-09 14:51:20 -0600

    Finished

--- src/models/Job.js
+++ src/models/Job.js
@@ -3,12 +3,11 @@ import { Schema } from "mongoose";
 
 
 export const JobSchema = new Schema({
-  bedrooms: { type: Number, required: true, min: 0, max: 30 },
-  bathrooms: { type: Number, required: true, min: 0, max: 25 },
-  levels: { type: Number, required: true, min: 1, max: 4 },
-  price: { type: Number, required: true, min: 0, max: 1000000 },
+  company: { type: String, required: true, minLength: 0, maxLength: 100 },
+  jobTitle: { type: String, required: true, minLength: 0, maxLength: 100 },
+  hours: { type: Number, required: true, min: 1, max: 168 },
+  rate: { type: Number, required: true, min: 0, max: 1000000 },
   imgUrl: { type: String, required: true, maxLength: 500 },
   description: { type: String, required: true, maxLength: 500 },
-  year: { type: Number, required: true, min: 1000, max: 2024 },
   creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' }
 }, { timestamps: true, toJSON: { virtuals: true } })
